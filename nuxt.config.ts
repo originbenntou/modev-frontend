987d6c098fd4d67794f412a611532efb96b6f642
mototsuka <originbenntou8973@gmail.com>
2023-05-16 22:31:31 +0900

    save

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  nitro: {
+    preset: 'node',
+  },
+  vite: {
+    server: {
+      watch: {
+        usePolling: true
+      }
+    },
+  },
+  devServer: {
+    host: '0.0.0.0',
+  },
+  // srcDir: 'src',
+  typescript: {
+    tsConfig: {
+      extends: '@tsconfig/strictest/tsconfig.json',
+    },
+  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt'
+  ],
 })
